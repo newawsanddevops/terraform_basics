cb3db8810adcc07dab0819cfa75b34637928ab85
sandeepgit52988 <needawsanddevops@gmail.com>
2023-09-26 18:18:09 +0530

    added

--- main.tf
+++ main.tf
@@ -1,14 +1,7 @@
-resource "local_file" "mypet" {
-    filename = "/root/terraform_basics/file.txt"
-    content =  "I love pets"
-}
-
-resource "random_pet" "mynewpet" {
-  length    = "3"
-  prefix    = "Mr"
-  separator = ","
+resource "aws_iam_user" "adminuser" {
+  user = "Indian"
+  tag = {
+    description = "I am an Indian"
   }
 
-  resource "random_id" "id" {
-    byte_length = "4"
- }
+}
