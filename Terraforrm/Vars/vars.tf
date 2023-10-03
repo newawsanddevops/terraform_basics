4aeaaf63b5f7dc70e2430dd5620b9fa38a91dc29
sandeepgit52988 <needawsanddevops@gmail.com>
2023-10-03 15:25:26 +0530

    added

--- Terraforrm/Vars/vars.tf
+++ Terraforrm/Vars/vars.tf
@@ -6,3 +6,19 @@ output "sample-one" {
 
     value = "var.sample"
 }
+
+variable "Sample_variable" {
+
+    default = [
+        "AWS",
+        123
+        "Azure"
+        456
+        "Gcp"
+    ]
+}
+
+output "Example_01"{
+
+    value = "${var.Sample_variable[0]} and ${var.Sample_variable[2]} with value of ${var.Sample_variable[1]}"
+}
