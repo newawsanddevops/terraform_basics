0f242c8210df300d3e2ec69bbfdb3be2c2715deb
sandeepgit52988 <needawsanddevops@gmail.com>
2023-10-04 11:57:24 +0530

    added

--- Terraforrm/sample/varss.tf
+++ Terraforrm/sample/varss.tf
@@ -9,3 +9,9 @@ variable "state" {}
 output "state_name" {
     value = "My state name is ${var.state}"
 }
+
+variable "cold_city" {}
+
+output "cold_city_name" {
+    value = "My cold city is ${var.cold_city}"
+}
