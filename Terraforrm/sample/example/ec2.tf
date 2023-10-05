c69d575538bc867516725d206d6f3876892b9f2a
sandeepgit52988 <needawsanddevops@gmail.com>
2023-10-05 13:05:37 +0530

    added

--- Terraforrm/sample/example/ec2.tf
+++ Terraforrm/sample/example/ec2.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "web" {
   ami           = "ami-067d1e60475437da2"
-  instance_type = "t3.micro"
+  instance_type = "t2.micro"
 
   tags = {
-    Name = "HelloWorld"
+    Name = "Hello World"
   }
 }
