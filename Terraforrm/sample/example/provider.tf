4f9b716c3080393e1c6654569415a09a08f5e724
sandeepgit52988 <needawsanddevops@gmail.com>
2023-10-05 13:14:03 +0530

    added

--- Terraforrm/sample/example/provider.tf
+++ Terraforrm/sample/example/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region = "us-east-1"
-  access_key = "AKIAYOVJX6HPVAQ5GKB5"
-  secret_key = "hWkJRU2ur9y3uh2CwC5zydPbhR9BxKjunnjmM7Sn"
+  access_key = "AKIAYOVJX6HPSNS23K7V"
+  secret_key = "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"
 }
