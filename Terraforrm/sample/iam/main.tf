0bc5a3f597d7e742e4b372f7d6b2fe2a39a75f52
sandeepgit52988 <needawsanddevops@gmail.com>
2023-10-06 11:13:10 +0530

    added

---
+++ Terraforrm/sample/iam/main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+    region = "us-east-1"
+    access_key = "AKIAYOVJX6HPS27NSUXK"
+    secret_key = "yTVITp1F0SIyj9Fkmub4XspGoRrfVMaJVCcCkhPI"
+}
+
+resource "aws_iam_users" "admin" {
+   name = "unravel"
+   tags = {
+    Description = "This is unravel"
+   }
+
+
+}
